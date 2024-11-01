2b3dd3bb53927b67d777267b036df8391a08f8aa
AmanMahor21 <amanmahor021@gmail.com>
2024-11-01 17:06:43 +0530

    event open above

--- src/components/EventDropDown.jsx
+++ src/components/EventDropDown.jsx
@@ -1,25 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { invertoryContext } from "../Pages/AddInventory";
 import { EVENT_LIST } from "../helpers/const";
 import Select from "react-select";
+
 const EventDropDown = ({ cloneDropDown }) => {
-  const { CustomOption, customStyle, selected } =
-    useContext(invertoryContext);
+  const { CustomOption, customStyle, selected } = useContext(invertoryContext);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const handleMenuOpen = () => {
+    setMenuIsOpen(true);
+  };
+
+  const handleMenuClose = () => {
+    setMenuIsOpen(false);
+  };
+
   return (
-    <>
-      <Select
-        className="basic-single pt-2 pb-2"
-        classNamePrefix="select"
-        styles={customStyle}
-        isDisabled={selected}
-        components={{ Option: CustomOption }}
-        options={EVENT_LIST}
-        onChange={cloneDropDown}
-        noOptionsMessage={() => {
-          return <div className="text-body-secondary">Not Found</div>;
-        }}
-      />
-    </>
+    <Select
+      className="basic-single pt-2 pb-2"
+      classNamePrefix="select"
+      styles={customStyle}
+      isDisabled={selected}
+      components={{ Option: CustomOption }}
+      options={EVENT_LIST}
+      onChange={cloneDropDown}
+      menuIsOpen={menuIsOpen}
+      onMenuOpen={handleMenuOpen}
+      onMenuClose={handleMenuClose}
+      noOptionsMessage={() => (
+        <div className="text-body-secondary">Not Found</div>
+      )}
+      menuPortalTarget={document.body} 
+    />
   );
 };
 
